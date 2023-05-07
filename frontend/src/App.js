63e6204887d0b80a941e7d1c192e0e127a949142
Rishabh Kumar <rishabhkaushal1796@gmail.com>
2023-05-07 11:14:23 +0530

    perform-api-call

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,46 @@
-import { Box, TextField, Typography } from "@mui/material";
+import { Box, Button, TextField, Typography } from "@mui/material";
 import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [text, setText] = useState("");
+  const [sentiment, setSentiment] = useState("");
+  const [confidence, setConfidence] = useState("");
+
+  const performAPICall = async () => {
+    let response = {};
+    try {
+      response = await (
+        await fetch("http://localhost:8082", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            text: text,
+          }),
+        })
+      ).json();
+      console.log(response);
+      setPredictions(response.toFixed(4));
+    } catch (e) {
+      return e;
+    }
+  };
+
+  const setPredictions = (response) => {
+    if (response <= 0.6) setSentiment("Negative");
+    if (response > 0.6 && response < 0.65) setSentiment("Neutral");
+    if (response >= 0.65) setSentiment("Positive");
+
+    setConfidence(response);
+  };
+
+  useEffect(() => {
+    setConfidence("");
+    setPredictions("");
+  }, [text]);
+
   return (
     <div className="App">
       <Box
@@ -10,14 +49,15 @@ function App() {
           height: "90px",
           backgroundImage:
             "linear-gradient(to right, #0f0c29, #302b63, #24243e)",
+          boxShadow: 3,
         }}
       >
         <h1
           style={{
             fontSize: 35,
             background: "-webkit-linear-gradient(#eee, #fff)",
-            webkitBackgroundClip: "text",
-            webkitTextFillColor: "transparent",
+            WebkitBackgroundClip: "text",
+            WebkitTextFillColor: "transparent",
           }}
         >
           Sentiment Analysis
@@ -25,7 +65,7 @@ function App() {
       </Box>
       <Box
         display="flex"
-        justifyContent="center"
+        justifyContent="flex-start"
         alignItems="center"
         flexDirection="column"
         gap={3}
@@ -35,27 +75,51 @@ function App() {
           height: "50vh",
           backgroundImage:
             "radial-gradient(circle farthest-side, #fceabb, #f8b500)",
+          boxShadow: 3,
         }}
       >
-        <Box>
+        <Box
+          marginTop="40px"
+          display="flex"
+          flexDirection="column"
+          justifyContent="center"
+          alignItems="center"
+        >
           <Typography sx={{ fontSize: 25, fontWeight: "700" }}>
             Input Text
           </Typography>
           <TextField
-            sx={{ background: "#ffffff", width: "500px" }}
+            value={text}
+            sx={{ background: "#ffffff", width: "500px", marginBottom: "10px" }}
             multiline
             rows={2}
             inputProps={{
               sx: { fontSize: 25 },
             }}
+            onChange={(e) => setText(e.target.value)}
           ></TextField>
+          <Button
+            variant="contained"
+            onClick={() => performAPICall()}
+            sx={{
+              fontSize: 15,
+              background:
+                "linear-gradient(to right, #0f0c29, #302b63, #24243e)",
+            }}
+          >
+            Calculate
+          </Button>
         </Box>
-        <Typography sx={{ fontSize: 25, fontWeight: "700" }}>
-          Sentiment: Positive
-        </Typography>
-        <Typography sx={{ fontSize: 25, fontWeight: "700" }}>
-          Confidence: 0.8501
-        </Typography>
+        {confidence && (
+          <Box>
+            <Typography sx={{ fontSize: 25, fontWeight: "700" }}>
+              Sentiment: {sentiment}
+            </Typography>
+            <Typography sx={{ fontSize: 25, fontWeight: "700" }}>
+              Confidence: {confidence}
+            </Typography>
+          </Box>
+        )}{" "}
       </Box>
     </div>
   );
